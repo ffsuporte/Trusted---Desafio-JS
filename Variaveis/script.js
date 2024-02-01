b893d09e2c261a8c25add353bde45295a9c5719b
ffsuporte <ffsuporte.ipb@gmail.com>
2024-02-01 01:44:13 -0300

    Exercicio  Variaveis

---
+++ Variaveis/script.js
@@ -0,0 +1,8 @@
+let nome = "Fernando";
+let sobrenome = "Fernandes";
+let idade = 50;
+let isBrasileiro = true;
+
+console.log(nome + " " + sobrenome);
+console.log(idade);
+console.log("É  Brasileiro ? " + isBrasileiro);
